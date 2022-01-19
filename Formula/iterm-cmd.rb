61c0f01b2e9d0149f1a628273e5bd8feaebc840a
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-19 17:09:10 -0600

    Brew formula update for iterm-cmd version v1.0.4

--- Formula/iterm-cmd.rb
+++ Formula/iterm-cmd.rb
@@ -5,21 +5,21 @@
 class ItermCmd < Formula
   desc "Generate files that contain commands and be cmd-clicked inside iTerm2"
   homepage "https://github.com/oliverisaac/iterm-cmd"
-  version "1.0.3"
+  version "1.0.4"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/oliverisaac/iterm-cmd/releases/download/v1.0.3/iterm-cmd_1.0.3_darwin_arm64.tar.gz"
-      sha256 "017787ff12cd822275c9731d42ea533fa018284d79e52411f0a2aff678488149"
+      url "https://github.com/oliverisaac/iterm-cmd/releases/download/v1.0.4/iterm-cmd_1.0.4_darwin_arm64.tar.gz"
+      sha256 "f620f6131b25394756c1f0721f2a2139b3db59b9f749cb9ec7b4d7701c3d4ca9"
 
       def install
         bin.install "iterm-cmd"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/oliverisaac/iterm-cmd/releases/download/v1.0.3/iterm-cmd_1.0.3_darwin_x86_64.tar.gz"
-      sha256 "e06dda01cf0e308ec701b8521bae52fe9f6e8b8a0acdcba54840f2994bc002c8"
+      url "https://github.com/oliverisaac/iterm-cmd/releases/download/v1.0.4/iterm-cmd_1.0.4_darwin_x86_64.tar.gz"
+      sha256 "a8c9bd6a414ee5bd2323c2d1729e0fa871c351ffd44a17d02cd483b2f60670a0"
 
       def install
         bin.install "iterm-cmd"
