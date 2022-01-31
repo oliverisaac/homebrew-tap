8f429b16b02868e21a3f03b53f781db76e61c859
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-30 22:43:18 -0600

    Brew formula update for koi version v0.0.7

--- Formula/koi.rb
+++ Formula/koi.rb
@@ -5,21 +5,21 @@
 class Koi < Formula
   desc "Kubectl wrapper command"
   homepage "https://github.com/oliverisaac/koi"
-  version "0.0.6"
+  version "0.0.7"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/oliverisaac/koi/releases/download/v0.0.6/koi_0.0.6_darwin_arm64.tar.gz"
-      sha256 "a9a2fbad03fe5b55127be65ea72021a127bf36fd3b1e0cf286c8538e668da59f"
+      url "https://github.com/oliverisaac/koi/releases/download/v0.0.7/koi_0.0.7_darwin_arm64.tar.gz"
+      sha256 "8fe48801dd652cf5cff7542acbb86216b5961e9e1961221eb2c178c98caa3060"
 
       def install
         bin.install "koi"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/oliverisaac/koi/releases/download/v0.0.6/koi_0.0.6_darwin_x86_64.tar.gz"
-      sha256 "f3101fef3b0cab16c465a4a46add2e5004292160f502fc119364a804be8e1304"
+      url "https://github.com/oliverisaac/koi/releases/download/v0.0.7/koi_0.0.7_darwin_x86_64.tar.gz"
+      sha256 "385cbeb02c348399b0d9ed8f7a38f15480c927b54cb3d249bf8aaddcd0e330b9"
 
       def install
         bin.install "koi"
