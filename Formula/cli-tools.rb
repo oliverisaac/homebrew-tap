bfefe991e4ce607d6078ad37be4243499d5763ad
Oliver Isaac (Automated) <oisaac+github@gmail.com>
2022-09-16 01:26:08 +0000

    Update to cli-tools.rb

--- Formula/cli-tools.rb
+++ Formula/cli-tools.rb
@@ -1,1 +1,9 @@
+class CliTools < Formula
+  head "github.com/oliverisaac/cli-tools", :using => :git
 
+  depends_on "coreutils" 
+  depends_on "oliverisaac/koi" 
+  depends_on :bash = >"5" 
+  depends_on "jq" 
+
+end
